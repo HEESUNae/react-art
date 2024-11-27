625298c3f7fc203f11830a902da9b4434ea660bb
Hee Sun Hwang <heesun9456@gmail.com>
2024-11-27 14:28:32 +0900

    Add files via upload

--- instance.ts
+++ instance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://api.kcisa.kr',
+  // baseURL: 'http://api.kcisa.kr',
+  baseURL: '/api',
   timeout: 1000,
 });
